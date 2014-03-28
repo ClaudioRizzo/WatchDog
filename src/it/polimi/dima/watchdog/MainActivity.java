bbf1fb769b447737b14122399e9404282c952707
ClaudioRizzo <claudio.rizzo.90@gmail.com>
2014-03-29 00:47:25 +0100

    Drawer Menu
    creato il drawer menù retrocompatibile con le API 8 (testato e funzionante sul mio ace 2.3.7), al momento c'è solo lo scheletro

--- src/it/polimi/dima/watchdog/MainActivity.java
+++ src/it/polimi/dima/watchdog/MainActivity.java
@@ -1,8 +1,10 @@
 package it.polimi.dima.watchdog;
 
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,16 +12,66 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-//prova
 public class MainActivity extends ActionBarActivity {
+
+	private String[] mGeneralFeaturesTitles;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+		
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
+		mTitle = mDrawerTitle = getTitle();
+		mGeneralFeaturesTitles = getResources().getStringArray(
+				R.array.gen_features_array);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, mGeneralFeaturesTitles));
+
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /* Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /* Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+                System.out.println("[DEBUG] BELLAZ");
+                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+		
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+        if (savedInstanceState == null) {
+
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
@@ -29,10 +81,22 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu items for use in the action bar
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main, menu);
-	    return super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
+	
+	@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+		return true;
+        // If the nav drawer is open, hide action items related to the content view
+        /*boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);*/
+		
+		//per nascondere gli elementi nella barra applicativa qualora il drawer venga
+		//aperto
+    }
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -40,11 +104,32 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.pippo3) {
-			return true;
+		
+		// true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+		
+		if (id == R.id.settings_panel) {
+			System.out.println("Ho cliccato il setting panel");
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+	
+	@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -61,6 +146,19 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+
+	/* Drawer List listener */
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			System.out.println("[DEBUG] ho cliccato");
+
+		}
+
 	}
 
 }
