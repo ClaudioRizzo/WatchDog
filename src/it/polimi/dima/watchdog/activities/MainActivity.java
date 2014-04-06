1f1c6d7a65208b7fb5dafaed9971ac01f941ccc3
ClaudioRizzo <claudio.rizzo.90@gmail.com>
2014-04-06 23:52:32 +0200

    Redesign applicazione
    design applicazione rivisto:
    macro aree selezionate dalla action bar mentre le sotto aree risiedono nelle tabs non c'è bisogno di alcun drawer

--- src/it/polimi/dima/watchdog/activities/MainActivity.java
+++ src/it/polimi/dima/watchdog/activities/MainActivity.java
@@ -1,237 +1,187 @@
 package it.polimi.dima.watchdog.activities;
 
-import java.util.List;
-
-import it.polimi.dima.watchdog.Consts;
 import it.polimi.dima.watchdog.R;
-import it.polimi.dima.watchdog.fragments.GpsFragment;
-import it.polimi.dima.watchdog.fragments.MainFeaturesFragment;
-import it.polimi.dima.watchdog.fragments.MainFeaturesFragment.OnFeatureSelectedListener;
-import it.polimi.dima.watchdog.fragments.PhoneStatusFragment;
-import it.polimi.dima.watchdog.fragments.SmsFragment;
+import it.polimi.dima.watchdog.fragments.FirstTabRootFragment;
+import it.polimi.dima.watchdog.fragments.LocalizatoionFragment;
+import it.polimi.dima.watchdog.fragments.PerimeterFragment;
+import it.polimi.dima.watchdog.fragments.SecondTabRootFragment;
+import it.polimi.dima.watchdog.fragments.SirenOnFragment;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity implements
-		OnFeatureSelectedListener {
+public class MainActivity extends ActionBarActivity implements TabListener {
 
-	private String[] mGeneralFeaturesTitles;
-	private DrawerLayout mDrawerLayout;
-	private ListView mDrawerList;
-
-	private ActionBarDrawerToggle mDrawerToggle;
-	private CharSequence mDrawerTitle;
-	
+	private TabsAdapter mTabsAdapter;
+	private ViewPager mViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main_layout);
 
-		mDrawerTitle = getTitle();
-		mGeneralFeaturesTitles = getResources().getStringArray(
-				R.array.gen_features_array);
+		this.setMyAdapter(getSupportFragmentManager(),
+				(ViewPager) findViewById(R.id.main_pager));
 
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-		// Set the adapter for the list view
-		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-				R.layout.drawer_list_item, mGeneralFeaturesTitles));
-
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-		// enable ActionBar app icon to behave as action to toggle nav drawer
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
-
-		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-				R.drawable.ic_drawer, R.string.drawer_open,
-				R.string.drawer_close) {
-
-			/* Called when a drawer has settled in a completely closed state. */
-			public void onDrawerClosed(View view) {
-				super.onDrawerClosed(view);
-				int cTitle = getCurrentFragmentTitle();
-				if(cTitle != R.string.app_name)
-					getSupportActionBar().setTitle(cTitle);
-				supportInvalidateOptionsMenu(); // creates call to
-												// onPrepareOptionsMenu()
-			}
-
-			/* Called when a drawer has settled in a completely open state. */
-			public void onDrawerOpened(View drawerView) {
-				super.onDrawerOpened(drawerView);
-				getSupportActionBar().setTitle(mDrawerTitle);
-				supportInvalidateOptionsMenu(); // creates call to
-												// onPrepareOptionsMenu()
-			}
-		};
-
-		mDrawerLayout.setDrawerListener(mDrawerToggle);
-		MainFeaturesFragment fragment = new MainFeaturesFragment();
-		Bundle args = new Bundle();
-		args.putInt(Consts.TITLE, R.string.app_name);
-		fragment.setArguments(args);
-
-		if (savedInstanceState == null) {
-
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.main_features_container, fragment, "").commit();
-		}
+		this.addTabs();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu items for use in the action bar
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
 
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		return true;
-		// If the nav drawer is open, hide action items related to the content
-		// view
-		/*
-		 * boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-		 * menu.findItem(R.id.action_websearch).setVisible(!drawerOpen); return
-		 * super.onPrepareOptionsMenu(menu);
-		 */
-
-		// per nascondere gli elementi nella barra applicativa qualora il drawer
-		// venga
-		// aperto
-	}
-
+	/**
+	 * use to handle menu item selected on the action bar
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
 
-		// true, then it has handled the app icon touch event
-		if (mDrawerToggle.onOptionsItemSelected(item)) {
+		FragmentTransaction trans = getSupportFragmentManager().beginTransaction();	
+		switch (item.getItemId()) {
+		case R.id.gps_icon:
+			trans.replace(R.id.tab1_root_frame, new LocalizatoionFragment());
+			trans.replace(R.id.tab2_root_frame, new PerimeterFragment());
+			trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+			trans.addToBackStack(null);
+			
+
+			trans.commit();
 			return true;
-		}
+		case R.id.sms_icon:
+			trans.replace(R.id.tab1_root_frame, new SirenOnFragment());
+			trans.replace(R.id.tab2_root_frame, new SirenOnFragment());
+			trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+			trans.addToBackStack(null);
 
-		if (id == R.id.settings_panel) {
-			System.out.println("Ho cliccato il setting panel");
+			trans.commit();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
-		// Sync the toggle state after onRestoreInstanceState has occurred.
-		mDrawerToggle.syncState();
+
 	}
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
-		mDrawerToggle.onConfigurationChanged(newConfig);
+
 	}
 
-	
+	private void setMyAdapter(FragmentManager fm, ViewPager v) {
+		mTabsAdapter = new TabsAdapter(fm);
+		mViewPager = v;
+		mViewPager.setAdapter(mTabsAdapter);
 
-	/* Drawer List listener */
-	private class DrawerItemClickListener implements
-			ListView.OnItemClickListener {
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between different app sections, select the corresponding tab.
+                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
+                // Tab.
+                getSupportActionBar().setSelectedNavigationItem(position);
+            }
+        });
+		
+		
+		// For each of the sections in the app, add a tab to the action bar.
+		for (int i = 0; i < mTabsAdapter.getCount(); i++) {
+			// Create a tab with text corresponding to the page title defined by
+			// the adapter.
+			// Also specify this Activity object, which implements the
+			// TabListener interface, as the
+			// listener for when this tab is selected.
+			ActionBar actionBar = getSupportActionBar();
+			actionBar
+					.addTab(actionBar.newTab()
+							.setText(mTabsAdapter.getPageTitle(i))
+							.setTabListener(this));
+		}
+
+	}
+
+	private void addTabs() {
+		ActionBar mActionBar = getSupportActionBar();
+
+		mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+	}
+
+	private class TabsAdapter extends FragmentPagerAdapter {
+
+		private String context = "GPS";
+
+		public TabsAdapter(String context, FragmentManager mFragmentManager) {
+			super(mFragmentManager);
+			this.setContext(context);
+		}
+
+		public TabsAdapter(FragmentManager fm) {
+			super(fm);
+		}
 
 		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
-			
-			switch (position) {
+		public Fragment getItem(int i) {
+
+			// handle gps tabs
+			switch (i) {
 			case 0:
-				mDrawerLayout.closeDrawers();
-				replaceFragment(new MainFeaturesFragment(), R.string.app_name);
-				break;
-			case 1:
-				mDrawerLayout.closeDrawers();
-				replaceFragment(new GpsFragment(), R.string.gps_title);
-				break;
-			case 2:
-				mDrawerLayout.closeDrawers();
-				replaceFragment(new SmsFragment(), R.string.remote_title);
-				break;
-			case 3:
-				mDrawerLayout.closeDrawers();
-				replaceFragment(new PhoneStatusFragment(), R.string.status_title);
-				break;
+				return new FirstTabRootFragment();
 			default:
-				mDrawerLayout.closeDrawers();
-				break;
+				return new SecondTabRootFragment();
 			}
-			
-			// qua gestiamo i click sugli elementi del menu' che appare a
-			// sinistra NB a seconda di avere un drawer in ogni attività
-			// possiamo pensare di avere una classe che gestisce il drawer
 
+		}
+
+		@Override
+		public int getCount() {
+
+			return 2;
+		}
+
+		public void setContext(String context) {
+			this.context = context;
 		}
 
 	}
 
 	@Override
-	public void onGpsFeatureSelected() {
-		replaceFragment(new GpsFragment(), R.string.gps_title);
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public void onPhoneStatusFeatureSelected() {
-		replaceFragment(new PhoneStatusFragment(), R.string.status_title);
-
+	public void onTabSelected(Tab tab, FragmentTransaction arg1) {
+		 mViewPager.setCurrentItem(tab.getPosition());
+		
 	}
 
 	@Override
-	public void onSmsRemoteFeatureSelected() {
-		replaceFragment(new SmsFragment(),R.string.remote_title);
-
-	}
-
-	
-	private void replaceFragment(Fragment frag, int title) {
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
 		
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		Bundle args = new Bundle();
-		args.putInt(Consts.TITLE, title);
-		frag.setArguments(args);
-		//Replace whatever is in the fragment_container view with this fragment,
-		// and add the transaction to the back stack so the user can navigate back
-		transaction.replace(R.id.main_features_container, frag);
-		transaction.addToBackStack(null);
-
-		// Commit the transaction
-		transaction.commit();
 	}
 
-	private int getCurrentFragmentTitle() {
-		FragmentManager fMan = getSupportFragmentManager();
-		List<Fragment> fragments = fMan.getFragments();
-		
-		for(Fragment f : fragments) {
-			if(f != null && f.isVisible()) {
-				return f.getArguments().getInt(Consts.TITLE);
-			} 
-		}
-		return -1;
-	}
+
+
 }
---
+++ src/it/polimi/dima/watchdog/fragments/LocalizatoionFragment.java
@@ -0,0 +1,18 @@
+package it.polimi.dima.watchdog.fragments;
+
+import it.polimi.dima.watchdog.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LocalizatoionFragment extends Fragment {
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_localization, container, false);
+       
+    }
+}
