91ab592b0074254cbfcc82bf8da27052319c4d03
Emanuele Uliana <cloudstrife9999@tiscali.it>
2014-04-16 14:54:34 +0200

    minor changes

---
+++ src/it/polimi/dima/watchdog/sms/SMSPublicKeyManagement.java
@@ -0,0 +1,15 @@
+package it.polimi.dima.watchdog.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SMSPublicKeyManagement extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
