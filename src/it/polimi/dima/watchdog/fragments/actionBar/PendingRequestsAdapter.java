30ead4a643e43a50661adb6628d5f7c01d464560
emanuele <emanuele@cloudstrife9999.cloudstrife9999>
2014-05-28 20:39:31 +0200

    Implementate domande e risposta simmetriche nel SMP

--- src/it/polimi/dima/watchdog/fragments/actionBar/PendingRequestsAdapter.java
+++ src/it/polimi/dima/watchdog/fragments/actionBar/PendingRequestsAdapter.java
@@ -1,6 +1,8 @@
 package it.polimi.dima.watchdog.fragments.actionBar;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -57,7 +59,7 @@ public class PendingRequestsAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View vi = convertView;
 		if (vi == null)
-			vi = inflater.inflate(R.layout.list_item_pending_recuests, null);
+			vi = inflater.inflate(R.layout.list_item_pending_requests, null);
 
 		LinearLayout mLinearLayout = (LinearLayout) vi
 				.findViewById(R.id.linear_layout_pending_list);
@@ -66,18 +68,28 @@ public class PendingRequestsAdapter extends BaseAdapter {
 				.findViewById(R.id.text_view_pending1);
 		SocialistRequestWrapper current = data.get(position);
 		setStringToShow(current, mTextView);
-
+		
+		
+		
 		EditText secAnswerEditText = (EditText) mLinearLayout
 				.findViewById(R.id.edit_text_secret_answer);
+		
+		EditText mySecQuestionEditText = (EditText) mLinearLayout
+				.findViewById(R.id.edit_text_my_secret_question);
+		
+		EditText mySecAnswerEditText = (EditText) mLinearLayout
+				.findViewById(R.id.edit_text_my_secret_answer);
+		
+		List<EditText> editTextList = new ArrayList<EditText>(Arrays.asList(secAnswerEditText, mySecQuestionEditText, mySecAnswerEditText));
 
 		Button refuseButton = (Button) mLinearLayout
 				.findViewById(R.id.button_refuse_smp);
 		Button sendButton = (Button) mLinearLayout
 				.findViewById(R.id.button_accept_smp);
 
-		handleRefuse(refuseButton, data.get(position).getNumber());
-		handleSend(sendButton, data.get(position).getNumber(),
-				secAnswerEditText);
+		handleRefuse(refuseButton, current.getNumber());
+		handleSend(sendButton, current.getNumber(),
+				editTextList);
 
 		return vi;
 	}
@@ -104,7 +116,7 @@ public class PendingRequestsAdapter extends BaseAdapter {
 		});
 	}
 
-	private void handleSend(Button sendButton, final String number, final EditText editText) {
+	private void handleSend(Button sendButton, final String number, final List<EditText> editTextList) {
 
 		sendButton.setOnClickListener(new OnClickListener() {
 
@@ -114,8 +126,15 @@ public class PendingRequestsAdapter extends BaseAdapter {
 				PublicKeyAutenticator pka = new PublicKeyAutenticator();
 				try {
 					//... poi ottengo ciò che ho digitato...
-					String secAnsw = editText.getText().toString();
+					String secAnsw = editTextList.get(0).getText().toString();
+					String mySecQuestion = editTextList.get(1).getText().toString();
+					String mySecAnswer = editTextList.get(2).getText().toString();
+					
 					Log.i("[DEBUG]", "[DEBUG_SMP] Ho cliccato send: " + secAnsw);
+					
+					//...poi setto nelle mie preferenze domanda e risposta segrete che ho scelto (saranno utilizzate dopo il giro di boa)...
+					MyPrefFiles.setMyPreference(MyPrefFiles.SECRET_Q_A, number + MyPrefFiles.SECRET_QUESTION, mySecQuestion, ctx);
+					MyPrefFiles.setMyPreference(MyPrefFiles.SECRET_Q_A, number + MyPrefFiles.SECRET_ANSWER, mySecAnswer, ctx);
 					
 					//... poi recupero la mia chiave pubblica...
 					pka.setMyPublicKey(MyPrefFiles.getMyPreference(MyPrefFiles.MY_KEYS, MyPrefFiles.MY_PUB, ctx));
