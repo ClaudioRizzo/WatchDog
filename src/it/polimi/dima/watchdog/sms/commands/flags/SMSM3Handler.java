e9fcc41dcaa29c04f2396d6e368401b4cca6998a
emanuele <emanuele@cloudstrife9999.cloudstrife9999>
2014-06-05 17:16:17 +0200

    minor

--- src/it/polimi/dima/watchdog/sms/commands/flags/SMSM3Handler.java
+++ src/it/polimi/dima/watchdog/sms/commands/flags/SMSM3Handler.java
@@ -28,8 +28,6 @@ import it.polimi.dima.watchdog.utilities.MyPrefFiles;
 import it.polimi.dima.watchdog.utilities.SMSUtility;
 
 public class SMSM3Handler implements SMSCommandVisitorInterface {
-<<<<<<< HEAD
-=======
 
 	private String other;
 	private Context ctx;
@@ -38,7 +36,6 @@ public class SMSM3Handler implements SMSCommandVisitorInterface {
 		this.other = other;
 		this.ctx = context;
 	}
->>>>>>> b1913dc576e07b450d467ef51f81b17b41d05e8c
 	
 	@Override
 	public void visit(SirenOnCodeMessage sirenOnCodeMessage) {
