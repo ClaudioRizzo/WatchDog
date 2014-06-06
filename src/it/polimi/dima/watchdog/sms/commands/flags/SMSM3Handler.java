531244025a754579ab44f65d880b7536bf66c06c
emanuele <emanuele@cloudstrife9999.cloudstrife9999>
2014-06-06 13:39:33 +0200

    debug

--- src/it/polimi/dima/watchdog/sms/commands/flags/SMSM3Handler.java
+++ src/it/polimi/dima/watchdog/sms/commands/flags/SMSM3Handler.java
@@ -118,7 +118,9 @@ public class SMSM3Handler implements SMSCommandVisitorInterface, LocationChangeL
 		Log.i("DEBUG", "DEBUG messaggio senza firma: lunghezza supposta = 38; lunghezza effettiva = " + messageWithoutSignature.length);
 		PrivateKey mPriv = MyPrefFiles.getMyPrivateKey(this.ctx);
 		byte[] signature = CryptoUtility.doSignature(messageWithoutSignature, mPriv);
+		Log.i("DEBUG", "DEBUG lunghezza della firma: " + signature.length);
 		byte[] message = packMessage(messageWithoutSignature, signature);
+		Log.i("DEBUG", "DEBUG lunghezza messaggio che va in pasto all'aes: " + message.length + "; dovrebbe essere uguale a " + messageWithoutSignature.length + " + " + signature.length);
 		Key encKey = fetchEncryptionKey();
 		byte[] iv = fetchIv();
 		AES256GCM encryptor = new AES256GCM(encKey, message, iv, CryptoUtility.ENC);
@@ -126,7 +128,7 @@ public class SMSM3Handler implements SMSCommandVisitorInterface, LocationChangeL
 		//Log.i("DEBUG", "DEBUG invio le a: "+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".getBytes().length);
 		//SMSUtility.sendCommandMessage(this.other, SMSUtility.COMMAND_PORT, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".getBytes());
 		Log.i("[DEBUG_COMMAND]", "[DEBUG_COMMAND] ciphertext = " + Base64.encodeToString(encryptor.getCiphertext(), Base64.DEFAULT));
-		Log.i("[DEBUG_COMMAND]", "[DEBUG_COMMAND] ciphertext length = " + encryptor.getCiphertext().length);
+		Log.i("[DEBUG_COMMAND]", "[DEBUG_COMMAND] ciphertext length = " + encryptor.getCiphertext().length + "; dovrebbe essere uguale a 16 + " + message.length);
 		SMSUtility.sendCommandMessage(this.other, SMSUtility.COMMAND_PORT, encryptor.getCiphertext());
 		Log.i("gps","gps m4 inviato");
 		MyPrefFiles.deleteUselessCommandSessionPreferencesAfterM4IsSent(this.other, this.ctx);
